0bf4adb1fcaf1b495aa88b7ae29314ebfc4da636
Paulo Aragao <paragao@amazon.com>
2025-09-17 12:07:13 +0100

    initial website commit

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import SpaceBetween from "@cloudscape-design/components/space-between";
+import AppLayoutToolbarPreview from './components/MainLayout';
+
+function App() {
+
+	return (
+		<SpaceBetween size="m">
+			<AppLayoutToolbarPreview />
+		</SpaceBetween>
+	);
+}
+
+export default App;
