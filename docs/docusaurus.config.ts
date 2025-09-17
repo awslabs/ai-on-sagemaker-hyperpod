d964f17d0236ca549fde8f6187bc9c4cfc1866ff
Paulo Aragao <paragao@amazon.com>
2025-09-17 12:12:26 +0100

    changed branch used to deploy gh pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,11 +19,13 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ai-on-sagemaker-hyperpod',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'awslabs', // Usually your GitHub org/user name.
   projectName: 'ai-on-sagemaker-hyperpod', // Usually your repo name.
+  deploymentBranch: 'website',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
