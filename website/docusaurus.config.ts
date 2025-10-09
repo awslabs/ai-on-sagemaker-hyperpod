8170be064192a9426bdd8313e3d19fbfda90005b
Paulo Aragao <5193870+paragao@users.noreply.github.com>
2025-10-09 21:11:10 +0100

    Update docusaurus.config.ts

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://awslabs.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ai-on-sagemaker-hyperpod/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
